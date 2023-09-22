78c82bca3343141e1e19e96b2ed8237abbb49857
benjebara amine <squaboi2001@gmail.comgit config --global user.name Squaboi>
2023-09-22 20:03:16 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Classcomponents from "./componentq/Classcomponents";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Classcomponents />
+      </div>
+      <div class="main"></div>
     </div>
   );
 }
---
+++ src/componentq/Classcomponents.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+export default class Classcomponents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "",
+      title: "CLICK BUTTON",
+      des: "",
+      job: "yehdek rabi ",
+    };
+
+    this.affichePhoto = () => {
+      this.setState({
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8_nEhlJOnoj1rd6tG-oJCM_T1eGMj-Qmf3w&usqp=CAU",
+      });
+      this.setState({
+        title: "Amine lim3alem",
+      });
+      this.setState({
+        des: "nhab dele3",
+      });
+      this.setState({
+        job: "injinieur",
+      });
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div class="card">
+          <div className="espace">
+            <img src={this.state.image} />
+            <h1>{this.state.title} </h1>
+            <h3>{this.state.des}</h3>
+            <h5>{this.state.job}</h5>
+          </div>
+        </div>
+        <button class="button-80" onClick={this.affichePhoto}>
+          Show
+        </button>
+      </div>
+    );
+  }
+}
